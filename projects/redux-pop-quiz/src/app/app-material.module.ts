824764f5605dfae1ec9aff39c56c4cb449b15eb9
Kobi Hari <kobi2294@yahoo.com>
2019-11-08 15:36:38 +0200

    Added toolbar

--- projects/redux-pop-quiz/src/app/app-material.module.ts
+++ projects/redux-pop-quiz/src/app/app-material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const modules = [
     MatButtonModule, 
-    MatIconModule
+    MatIconModule, 
+    MatToolbarModule
 ];
 
 @NgModule({
