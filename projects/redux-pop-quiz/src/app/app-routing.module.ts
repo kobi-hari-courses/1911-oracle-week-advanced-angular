875e50658e80bf12f41a21ceea025bd78d3f120a
Kobi Hari <kobi2294@yahoo.com>
2019-11-14 18:12:44 +0200

    configured router outlet

--- projects/redux-pop-quiz/src/app/app-routing.module.ts
+++ projects/redux-pop-quiz/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { QuizDoneComponent } from './components/quiz-done/quiz-done.component';
+import { QuestionPresenterComponent } from './components/question-presenter/question-presenter.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'question', pathMatch: 'full'}, 
+  { path: 'question', component: QuestionPresenterComponent}, 
+  { path: 'done', component: QuizDoneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
