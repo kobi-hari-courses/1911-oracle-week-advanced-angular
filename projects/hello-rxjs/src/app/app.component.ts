3511e9e78360ea03bf5b661f37be707b0e3c1048
Kobi Hari <kobi2294@yahoo.com>
2019-11-15 14:25:38 +0200

    Creating Observers

--- projects/hello-rxjs/src/app/app.component.ts
+++ projects/hello-rxjs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  createObserver(id: number): Observer<number> {
+    console.info(`Created observer ${id}`);
+    let res: Observer<number> = {
+      next: val => console.log(`Observer ${id} next: ${val}`), 
+      complete: () => console.log(`Observer ${id} completed`),
+      error: err => console.log(`Observer ${id} error: ${err}`) 
+    }
+    return res;
+  }
+
   start() {
-    console.log('Here we go');
+    let o1 = this.createObserver(1);
+    let o2 = this.createObserver(2);
   }
 }
