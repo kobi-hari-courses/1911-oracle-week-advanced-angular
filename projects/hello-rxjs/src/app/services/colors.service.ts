a305907826030eb066a2dc220d98da9d5480cd4f
Kobi Hari <kobi2294@yahoo.com>
2019-11-17 12:02:22 +0200

    Added Colors model, data and service

---
+++ projects/hello-rxjs/src/app/services/colors.service.ts
@@ -0,0 +1,30 @@
+import { ALLCOLORS } from './../models/all-colors';
+import { Injectable } from '@angular/core';
+import { ColorModel } from '../models/color.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColorsService {
+
+  private delay(millis: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, millis);
+    })
+  }
+
+  async search(filter: string): Promise<ColorModel[]> {
+    console.log(`starting search for ${filter}`);
+    
+    if (!filter) return [];
+
+    filter = filter.toLowerCase();
+    const entries = Object.entries(ALLCOLORS);
+    const res = entries
+      .map(entry => {return <ColorModel> {displayName: entry[1], code: entry[0]}})
+      .filter(color => color.displayName.toLowerCase().includes(filter));
+    
+    await this.delay(2000);
+    return res;
+  }
+}
